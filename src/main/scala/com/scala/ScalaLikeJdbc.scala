f275adf64d12fdf87a0c9239ee0cb65c69dac460
cindy <14489918@qq.com>
2018-07-20 11:05:23 +0800

    new repository

--- src/main/scala/com/scala/ScalaLikeJdbc.scala
+++ src/main/scala/com/scala/ScalaLikeJdbc.scala
@@ -1,10 +1,22 @@
 package com.scala
 
+import scalikejdbc._
+import scalikejdbc.config._
+
 object ScalaLikeJdbc {
 
   def main(args: Array[String]): Unit = {
 
-    println("hello scala!")
+    DBs.setupAll()
+
+    val memberIds = DB readOnly { implicit session =>
+      sql"select id from people".map(_.long(1)).list.apply()
+    }
+
+    println(memberIds)
+
+    DBs.closeAll()
+
   }
 
 }
